492dcea2c78496cb8aca5e67998b300a3d2b4fa3
vuclig <kikike.hekkerecske@gmail.com>
2026-03-29 16:44:54 +0200

    u

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-alert ('Szavazz a DUNÁRA!');
+alert ('Szavazz a DUNÁRA!1');
 console.log('Szavazz a DUNÁRA!');
 console.error('Szerintem nem szavaztál a Dunára...');
 console.warn('Szavazz a dunára!');
+let szoveg = 'Heló Ez baj';
